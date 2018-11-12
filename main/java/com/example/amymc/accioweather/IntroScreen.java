7ba5b152c98a9f5a64fe7e8983b7936014319ac2
AmyRose20 <33226039+AmyRose20@users.noreply.github.com>
2018-11-12 23:34:52 +0000

    Add files via upload
    Intro Screen for app created.

---
+++ main/java/com/example/amymc/accioweather/IntroScreen.java
@@ -0,0 +1,42 @@
+package com.example.amymc.accioweather;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class IntroScreen extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_screen);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+        imageView.startAnimation(animation);
+
+        Thread timer = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    sleep(8000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    super.run();
+                } catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        };
+        timer.start();
+    }
+}
